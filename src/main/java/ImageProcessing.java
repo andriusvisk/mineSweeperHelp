8e3a54079edba9612d2956c3c9c003fb782565c9
andrius <andriusvisk@gmail.com>
2021-10-21 18:00:37 +0300

    image processing

--- src/main/java/ImageProcessing.java
+++ src/main/java/ImageProcessing.java
@@ -7,8 +7,8 @@ import org.opencv.imgproc.Imgproc;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ImageProcessing {
 
@@ -18,23 +18,77 @@ public class ImageProcessing {
         Imgproc.GaussianBlur(dest, dest, new Size(5, 5), 0);
         Imgproc.adaptiveThreshold(dest, dest, 255, 1, 1, 11, 2);
 
-        dest = findContourss(dest);
+        dest = findContours(dest);
 
         return dest;
     }
 
-    public static Mat findContourss(Mat src){
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+    public static Mat findContours(Mat src) {
+        List<MatOfPoint> contours = new ArrayList<>();
         Imgproc.findContours(src, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        Map<Integer, List<Rect>> boundingRectByArea = new HashMap<>();
 
         Mat result = new Mat(src.size(), CvType.CV_8UC3, new Scalar(255, 255, 255));
         for (int i = 0; i < contours.size(); i++) {
-            List<Point> points = contours.get(i).toList();
-            //Rect rect = Imgproc.boundingRect(contours.get(i));
-            for (Point point : points) {
-                Imgproc.circle(result, point, 1, new Scalar(255,10,15));
+
+            Rect boundingRect = Imgproc.boundingRect(contours.get(i));
+            Integer area = (int) boundingRect.area();
+
+            if (area > 20) {
+
+                int closestArea = boundingRectByArea.keySet().stream()
+                        .min(Comparator.comparingInt(p -> Math.abs(p - area)))
+                        .orElse(-1);
+
+                int tolleranceInPercents = 15;
+
+                if (closestArea > 0 && closestArea / 100 * tolleranceInPercents >= Math.abs(closestArea - area)) {
+                    boundingRectByArea.get(closestArea).add(boundingRect);
+                } else {
+                    List<Rect> list = new ArrayList<>();
+                    list.add(boundingRect);
+                    boundingRectByArea.put(area, list);
+                }
+
             }
         }
+
+        boundingRectByArea = boundingRectByArea.entrySet().stream()
+                .filter(i -> i.getValue().size() >= 4)
+                .collect(Collectors.toMap(x -> x.getKey(), x -> x.getValue()));
+
+        Map<Integer, List<Rect>> boundingRectByLines = new HashMap<>();
+
+        int tolleranceInPercents = 15;
+
+        for (Integer key : boundingRectByArea.keySet()) {
+            for (Rect rect : boundingRectByArea.get(key)) {
+
+                Integer rectX = rect.x;
+
+                int closestX = boundingRectByLines.keySet().stream()
+                        .min(Comparator.comparingInt(p -> Math.abs(p - rectX)))
+                        .orElse(-1);
+
+                if (closestX > 0 && closestX / 100 * tolleranceInPercents >= Math.abs(closestX - rectX)) {
+                    boundingRectByLines.get(closestX).add(rect);
+                } else {
+                    List<Rect> list = new ArrayList<>();
+                    list.add(rect);
+                    boundingRectByLines.put(rectX, list);
+                    closestX = rectX;
+                }
+
+                List<Rect> list = boundingRectByLines.get(closestX);
+                for (int i = 0; i < list.size(); i++) {
+                    //Imgproc.drawContours(result, list, i, new Scalar(0, 255, 0));
+                    Imgproc.rectangle(result, list.get(i), new Scalar(0, 255, 0));
+                }
+            }
+        }
+
+
         return result;
     }
 
